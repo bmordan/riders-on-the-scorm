dbb6219bcae3fedfcb42870768bffb1faad41e46
bmordan <bernardmordan@gmail.com>
2020-02-21 10:44:11 +0000

    updates to page

--- src/Helppage.svelte
+++ src/Helppage.svelte
@@ -4,14 +4,13 @@ const intro = `
 # How to create a SCORM package
 
 SCORM is an acronym for Sharable Content Object Reference Model. A scorm package can be uploaded onto a compatable managed learning platform like [Moodle](https://moodle.org/). The advantage of a SCORM package is it enables you to interact with the learning platform, and they are easy to share and maintain. This tool lets you create SCORM packages using markdown.
-![new package](https://user-images.githubusercontent.com/4499581/74726224-00e66700-5237-11ea-864f-b8bd833950b3.png)
+
 To create a new package click on "New package" on your home screen. Click on the package to open the editor. In the editor you need to create your content using markdown:
 
 ## What is markdown?
 
 Markdown is a lightweight markup language with plain-text-formatting syntax. Its design allows it to be converted to many output formats. Why markdown? It makes a real difference to compose written content when you don't need to worry about formatting and spacing. Have you ever created content in a word processor like Google docs or Word, only to have all your formatting go skewiff when it's shared or opened on someone elses computer? It's also really hard to get consistancy over lots of different documents created over a longer time period.
 
-![john gruber and aaron swartz](https://pic3.zhimg.com/80/v2-6e4e7562bd6b61e6611654b6b2c3c3aa_hd.jpg)
 _[John Gruber](https://daringfireball.net/) created the Markdown language in 2004 in collaboration with [Aaron Swartz](https://en.wikipedia.org/wiki/Aaron_Swartz)._
 
 Markdown has you just focus on written amazing content. You can format your work without leaving the keyboard. Below is a short primer to have you armed with the basics of markdown.`
@@ -24,16 +23,14 @@ const headings = `# h1 Heading 8-)
 ###### h6 Heading
 `
 
-const horizontalrules = `
-___
+const horizontalrules = `___
 
 ---
 
 ***
 `
 
-const emphasis = `
-**This is bold text**
+const emphasis = `**This is bold text**
 
 __This is bold text__
 
@@ -44,11 +41,74 @@ _This is italic text_
 ~~Strikethrough~~
 `
 
+const blockquotes = `> Blockquotes can also be nested...
+>> ...by using additional greater-than signs right next to each other...
+> > > ...or with spaces between arrows.
+`
+
+const lists = `Unordered
+
++ Create a list by starting a line with "+", "-", or "*"
++ Sub-lists are made by indenting 2 spaces:
+  - Marker character change forces new list start:
+    * Ac tristique libero volutpat at
+    + Facilisis in pretium nisl aliquet
+    - Nulla volutpat aliquam velit
++ Very easy!
+
+Ordered
+
+1. Lorem ipsum dolor sit amet
+2. Consectetur adipiscing elit
+3. Integer molestie lorem at massa
+
+
+1. You can use sequential numbers...
+1. ...or keep all the numbers as "1."
+
+Start numbering with offset:
+
+57. foo
+1. bar
+`
+
+const links = `[link text](http://dev.nodeca.com)
+
+[link with title](http://nodeca.github.io/pica/demo/ "title text!")
+
+Autoconverted link https://github.com/nodeca/pica (enable linkify to see)`
+
+const images = `![Minion](https://octodex.github.com/images/minion.png)
+![Stormtroopocat](https://octodex.github.com/images/stormtroopocat.jpg "The Stormtroopocat")
+
+Like links, Images also have a footnote style syntax
+
+![Alt text][id]
+
+With a reference later in the document defining the URL location:
+
+[id]: https://octodex.github.com/images/dojocat.jpg  "The Dojocat"
+`
+
+const emojies = `
+Classic markup: :wink: :necktie: :cry: :laughing: :yum:
+
+Shortcuts (emoticons): :-) :-( 8-) ;)
+
+see [how to change output](https://github.com/markdown-it/markdown-it-emoji#change-output) with twemoji.
+`
+
 const sections = [
     ["Headings", headings],
     ["Horizontal Rules", horizontalrules],
-    ["Emphasis", emphasis]
+    ["Emphasis", emphasis],
+    ["Blockquotes", blockquotes],
+    ["Lists", lists],
+    ["Links", links],
+    ["Images", images],
+    ["Emojies", emojies]
 ]
+
 
 </script>
 <section>
